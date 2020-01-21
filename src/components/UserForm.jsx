be785316b886b8d2669c2f61276446d9006d19ee
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-01-21 17:46:35 -0300

    Adicionando campos de input e rotas

---
+++ src/components/UserForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+class UserForm extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Email do Gravatar</p>
+        <input type="text" placeholder="email" required />
+        <p>Nome do jogador</p>
+        <input type="text" placeholder="nome" required />
+      </div>
+    )
+  }
+}
+export default UserForm;
