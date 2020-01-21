81dabf3965e428ba63d9834181e4dd08949a5f70
richardwellerson <richardwellerson@gmail.com>
2020-01-21 18:13:24 -0300

    CC

--- src/services/gravatarAPI.js
+++ src/services/gravatarAPI.js
@@ -3,6 +3,6 @@ import MD5 from 'crypto-js/md5';
 const imageLink = (email) => {
   const hashReady = MD5(email);
   return `https://www.gravatar.com/avatar/${hashReady}`;
-}
+};
 
 export default imageLink;
