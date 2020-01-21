a1926fbdc388411f770ba0709df68ef8b1e7e200
Henrique Eyer <henriqueeyer@outlook.com>
2020-01-21 17:00:59 -0300

    FOi

--- src/services/TriviaApi.js
+++ src/services/TriviaApi.js
@@ -9,8 +9,6 @@ export const getTokenTriviaApi = () => (
     ))
 );
 
-
-
 const token = '9baac3a971523f8c946f196ae27b91d99e0f7fbf6c29e400c7c7017s0f3e5529d';
 const NumberAsks = 5;
 const ASKSTRIVIA_API = `https://opentdb.com/api.php?amount=${NumberAsks}&token=${token}`;
