73ae4a67aee0ddba2e0b0ac081c383641d94dfae
richardwellerson <richardwellerson@gmail.com>
2020-01-22 16:46:05 -0300

    Upando alteraçoes Code Climate Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { PropTypes, defaultProps } from 'prop-types';
 import './Header.css';
 
 const Header = ({
   srcLink = 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
   player = 'João',
   score = 20,
-  }) => (
+}) => (
     <div className="background-header">
       <div className="photo-and-name-player">
         <img src={srcLink} alt={`${player} gravatar`} />
@@ -20,7 +20,7 @@ const Header = ({
 export default Header;
 
 Header.propTypes = {
-  srcLink: PropTypes.string,
-  player: PropTypes.string,
-  score: PropTypes.number,
+  srcLink: PropTypes.string.isRequired,
+  player: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
 };
