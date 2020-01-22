617b9b67e7907b24acc6debdc820835eb74ee6cd
richardwellerson <richardwellerson@gmail.com>
2020-01-22 16:33:13 -0300

    Retornando app ao estado inicial]

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import UserForm from './components/UserForm';
 import './App.css';
-import Header from '../src/components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Header} />
+        <Route exact path="/" component={UserForm} />
         {/* <Route path="/settings" component={} />
         <Route path="/game" component={} />
         <Route path="/game/feedback" component={} />
