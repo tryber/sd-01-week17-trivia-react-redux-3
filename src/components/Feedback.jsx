71d887d6ecaee0daeb1cce889aedb4d2131bf6ac
Henrique Eyer <henriqueeyer@outlook.com>
2020-01-22 16:54:41 -0300

    iniciando feedback

---
+++ src/components/Feedback.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Feedback = () => (
+  <div className="Feedback">
+    <Header />
+    <TitleFeedback />
+    <TextFeedback />
+  </div>
+);
+
+export default Feedback;
