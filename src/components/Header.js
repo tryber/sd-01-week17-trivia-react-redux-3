a717a346739d6f1123ccc7fab05b0eb5da0216de
richardwellerson <richardwellerson@gmail.com>
2020-01-22 17:07:01 -0300

    inserindo datatest-id no playername

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({
   <div className="background-header">
     <div className="photo-and-name-player">
       <img src={srcLink} alt={`${player} gravatar`} />
-      <p>{`Jogador: ${player}`}</p>
+      <p data-testid="header-player-name">{`Jogador: ${player}`}</p>
     </div>
     <p data-testid="header-score">{`Pontos: ${score}`}</p>
   </div>
