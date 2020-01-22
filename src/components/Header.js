c129d47910b4b4b8e836ed06e7a206f613fab78f
richardwellerson <richardwellerson@gmail.com>
2020-01-22 16:49:14 -0300

    Code Climate

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,14 +7,14 @@ const Header = ({
   player = 'João',
   score = 20,
 }) => (
-    <div className="background-header">
-      <div className="photo-and-name-player">
-        <img src={srcLink} alt={`${player} gravatar`} />
-        <p>{`Jogador: ${player}`}</p>
-      </div>
-      <p data-testid="header-score">{`Pontos: ${score}`}</p>
+  <div className="background-header">
+    <div className="photo-and-name-player">
+      <img src={srcLink} alt={`${player} gravatar`} />
+      <p>{`Jogador: ${player}`}</p>
     </div>
-  );
+    <p data-testid="header-score">{`Pontos: ${score}`}</p>
+  </div>
+);
 
 
 export default Header;
