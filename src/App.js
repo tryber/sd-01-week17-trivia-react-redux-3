f7156ff287f5b7daf8e1a589ca13db8a05fb457e
richardwellerson <richardwellerson@gmail.com>
2020-01-22 16:32:30 -0300

    Corrigindo Code Climate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import UserForm from './components/UserForm';
-import './App.css'
+import './App.css';
+import Header from '../src/components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={UserForm} />
+        <Route exact path="/" component={Header} />
         {/* <Route path="/settings" component={} />
         <Route path="/game" component={} />
         <Route path="/game/feedback" component={} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Header.css';
 
 const Header = ({
   srcLink = 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
   player = 'João',
-  score = 20
-}) => {
-  return (
-    <div className='background-header'>
-      <div className='photo-and-name-player'>
-        <img src={srcLink} alt={`${player} gravatar`}></img>
+  score = 20,
+  }) => (
+    <div className="background-header">
+      <div className="photo-and-name-player">
+        <img src={srcLink} alt={`${player} gravatar`} />
         <p>{`Jogador: ${player}`}</p>
       </div>
       <p>{`Pontos: ${score}`}</p>
     </div>
   );
-}
+
 
 export default Header;
+
+Header.propTypes = {
+  srcLink: PropTypes.string,
+  player: PropTypes.string,
+  score: PropTypes.number,
+};
