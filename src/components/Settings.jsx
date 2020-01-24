7e5a62d9ad873dbfbb3ddcbebf14c7c3c19c3696
Henrique Eyer <henriqueeyer@outlook.com>
2020-01-24 15:39:34 -0300

    Terminado cc

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { addSettings } from '../actions';
@@ -7,7 +8,7 @@ import './Settings.css';
 
 class Settings extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       type: '',
       difficulty: '',
@@ -17,31 +18,30 @@ class Settings extends React.Component {
     this.changeCategory = this.changeCategory.bind(this);
     this.changeDifficulty = this.changeDifficulty.bind(this);
     this.changeType = this.changeType.bind(this);
-  };
+  }
 
   componentWillUnmount() {
     const { setSettings } = this.props;
     setSettings({ ...this.state });
-  };
+  }
 
   changeType(newValue) {
     this.setState({
       type: newValue,
     });
-  };
+  }
 
   changeDifficulty(newValue) {
     this.setState({
       difficulty: newValue,
     });
-  };
+  }
 
   changeCategory(newValue) {
     this.setState({
       category: newValue,
     });
-  };
-
+  }
 
   render() {
     return (
@@ -53,7 +53,7 @@ class Settings extends React.Component {
         <Link to="/" className="back" >Back</Link>
       </div>
     );
-  };
+  }
 };
 
 const mapDispatchToProps = (dispatch) => ({
@@ -61,3 +61,7 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export default connect(null, mapDispatchToProps)(Settings);
+
+Settings.propTypes = {
+  setSettings: PropTypes.func.isRequired,
+};
