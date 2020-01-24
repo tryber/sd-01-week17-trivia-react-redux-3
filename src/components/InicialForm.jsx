78622de1cad5f5b1bce0669927851d864e03a304
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-01-24 12:27:17 -0300

    adicionando mapState e mapDispatch

--- src/components/InicialForm.jsx
+++ src/components/InicialForm.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { ADD_NAME, ADD_EMAIL, ADD_LINK_IMAGE } from '../actions';
+import imageLink from '../services/gravatarAPI';
 
 const inicialForm = (props) => {
   const { valueEmail, valueName, onChangeValueEmail, onChangeValueName } = props;
@@ -29,11 +31,13 @@ const inicialForm = (props) => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
-
+  login: dispatch({type: ADD_NAME, type: ADD_EMAIL, type: ADD_LINK_IMAGE})
 });
 
 const mapStateToProps = (state) => ({
-  
+  name: state.valueName,
+  email: state.valueEmail,
+  linkImage: state.imageLink
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(inicialForm);
