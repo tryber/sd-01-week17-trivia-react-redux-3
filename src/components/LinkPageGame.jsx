9a5f9b55eeafb3183129b735a100dd257649b621
Henrique Eyer <henriqueeyer@outlook.com>
2020-01-24 17:18:38 -0300

    Corrigindo o login page para ficar de acordo com o requisito

--- src/components/LinkPageGame.jsx
+++ src/components/LinkPageGame.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchToken } from '../actions';
 import './LinkPageGame.css';
 
 class LinkPageGame extends React.Component {
-  componentDidMount() {
-    const { getToken } = this.props;
-    getToken();
-  }
-
   render() {
-    const { disable } = this.props;
-    if (!disable) return <h3>Preencha os campos selecionados</h3>;
+    const { disable, getToken, isFetching, token } = this.props;
+    if (token) return <Redirect to="/game" />
     return (
-      <Link to="/game" className="link-game" data-testid="btn-play" disabled={disable}>
-        Iniciar o jogo
-      </Link>
+      <div>
+        <input
+          type="button"
+          value={(isFetching ? "Carregando" : "Jogar")}
+          className="link-game"
+          onClick={() => getToken()}
+          data-testid="btn-play"
+          disabled={!disable}
+        />
+      </div >
     );
   }
 }
@@ -26,8 +28,12 @@ const mapDispatchToProps = (dispatch) => ({
   getToken: () => dispatch(fetchToken()),
 });
 
+const mapStateToProps = ({ Token: { token, isFetching } }) => ({
+  token,
+  isFetching,
+});
 
-export default connect(null, mapDispatchToProps)(LinkPageGame);
+export default connect(mapStateToProps, mapDispatchToProps)(LinkPageGame);
 
 LinkPageGame.propTypes = {
   disable: PropTypes.bool.isRequired,
