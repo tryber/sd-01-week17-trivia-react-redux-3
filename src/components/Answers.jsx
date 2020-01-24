b44ab7423599888bc674ea53ff1f53e9f717ad9e
richardwellerson <richardwellerson@gmail.com>
2020-01-24 16:42:33 -0300

    Code Climater

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import ButtonAnswer from './ButtonAnswers';
-import './Answers.css'
+import PropTypes from 'prop-types';
+import './Answers.css';
 
 const Answers = ({
   correct = 'Será',
   incorrects = ['Ele é', 'Voará', 'As pessoas boas devem amar seus inimigos'],
 }) => {
-  const allAnswers = [...incorrects]
+  const allAnswers = [...incorrects];
   const randomIndex = Math.floor(Math.random() * 3.49);
   allAnswers.splice(randomIndex, 0, correct);
   return (
     <div className="Answers_all">
       {allAnswers.map((answer) => (
-        <ButtonAnswer key={answer} value={answer} correctAnswer={correct}/>
+        <ButtonAnswer key={answer} value={answer} correctAnswer={correct} />
       ))}
     </div>
   );
-}
+};
 
 export default Answers;
+
+Answers.propTypes = {
+  correct: PropTypes.string,
+  incorrects: PropTypes.array.isRequired,
+}
--- src/components/ButtonAnswers.jsx
+++ src/components/ButtonAnswers.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ButtonAnswers = ({ value, correctAnswer }) => {
 return <button className="Answer_button">{value}</button>
 }
 
 export default ButtonAnswers;
+
+ButtonAnswers.propTypes = {
+  value: PropTypes.string,
+  correctAnswer: PropTypes.string,
+}
