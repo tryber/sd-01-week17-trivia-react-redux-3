df47af18fbf0cacc222ae0703470825a40cf004c
Henrique Eyer <henriqueeyer@outlook.com>
2020-01-27 00:56:37 -0300

    Quase finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import './Header.css';
 
 const Header = ({
-  srcLink = 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
-  player = 'João',
-  score = 20,
+  srcLink,
+  player,
+  score,
 }) => (
-  <div className="Header_background-header">
-    <div className="Header_photo-and-name-player">
-      <img
-        className="Header_photo"
-        src={srcLink}
-        alt={`${player} gravatar`}
-      />
+    <div className="Header_background-header">
+      <div className="Header_photo-and-name-player">
+        <img
+          className="Header_photo"
+          src={srcLink}
+          alt={`${player} gravatar`}
+        />
+        <p
+          className="Header_paragraph"
+          data-testid="header-player-name"
+        >
+          {`Jogador: ${player}`}
+        </p>
+      </div>
       <p
         className="Header_paragraph"
-        data-testid="header-player-name"
+        data-testid="header-score"
       >
-        {`Jogador: ${player}`}
+        {`Pontos: ${score}`}
       </p>
     </div>
-    <p
-      className="Header_paragraph"
-      data-testid="header-score"
-    >
-      {`Pontos: ${score}`}
-    </p>
-  </div>
-);
+  );
 
-export default Header;
+const mapStateToProps = ({ User: { name, linkImage, score } }) => ({
+  player: name,
+  srcLink: linkImage,
+  score,
+});
+
+export default connect(mapStateToProps)(Header);
 
 Header.propTypes = {
   srcLink: PropTypes.string,
@@ -39,7 +46,7 @@ Header.propTypes = {
 };
 
 Header.defaultProps = {
-  srcLink: undefined,
-  player: undefined,
-  score: undefined,
+  srcLink: '',
+  player: '',
+  score: '',
 };
