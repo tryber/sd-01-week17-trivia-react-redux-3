3eedae4e8c7fc77791233f70b0d2e9ae8103ff27
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-01-26 21:04:07 -0300

    adicionando testes para a pagina de Login

--- src/tests/LoginPage.test.js
+++ src/tests/LoginPage.test.js
@@ -1,25 +1,53 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import App from '../App';
-import { Router } from 'react-router-dom';
 import { render, fireEvent, cleanup } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
-
-function renderWithRouter(
-  ui,
-  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
-) {
-  return {
-    ...render(<Router history={history}>{ui}</Router>),
-    history,
-  };
-}
+import store from '../store/index';
 
 describe('Elements at the screen / ', () => {
   afterEach(cleanup);
   
   test('texts in the screen, (Email do gravatar, Nome do jogador, Jogar', () => {
-    const { getByText } = renderWithRouter(<App />);
-    
-    expect(getByText(/Email/i)).toBeInTheDocument();
+    const { getByText,debug, getAllByAltText } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    );
+
+    expect(getByText(/Email do Gravatar/i)).toBeInTheDocument();
+    expect(getByText('Nome do jogador')).toBeInTheDocument();
+
+    const input = document.getElementsByTagName('input');
+
+    fireEvent.change(input[0], { target: { value: 'Player'} });
+    expect(input[0].value).toBe('Player');
+
+    fireEvent.change(input[1], { target: { value: 'player@gmail.com'} });
+    expect(input[1].value).toBe('player@gmail.com');
+
+    fireEvent.click(getByText('Jogar'));
+    expect(getByText(/Carregando/i)).toBeInTheDocument();
+    debug()
+
+    //falta colocar o teste demonstrando que o esta sendo redirecionado para a pagina de '/game'
+
+    // const img = getAllByAltText(`${nomeDoJogador} gravatar`);
+    // expect(img).toBeInTheDocument();
+    // expect(img.src).toBe(`https://www.gravatar.com/avatar/${tokenDoJogador}`);
+    // expect(getByText(`Jogador: ${nomeDoJogador}`)).toBeInTheDocument();
+    // expect(getByText(`Pontos: ${pontosDoJogador}`)).toBeInTheDocument();
+  });
+
+  test('redirect to settings page', () => {
+    const { getByText, debug } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    );
+
+    const buttonSettings = document.getElementsByClassName('button-settings')[0];
+
+    fireEvent.click(buttonSettings);
+    expect(getByText(/Configurações/i)).toBeInTheDocument();
   });
 });
