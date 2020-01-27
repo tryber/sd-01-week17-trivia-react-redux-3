11d6b00061d2bc8882bb2fbd845ad7af175344f8
Henrique Eyer <henriqueeyer@outlook.com>
2020-01-27 01:38:01 -0300

    Tela de feedback pronta

--- src/components/PageGame.jsx
+++ src/components/PageGame.jsx
@@ -73,7 +73,6 @@ class PageGame extends React.Component {
   render() {
     const { history: { action } } = this.props;
     const { contQuestion, response } = this.state;
-    console.log(contQuestion);
     if (action === 'POP') return <Redirect to="/" />;
     if (response === 3) return <Redirect to="/" />;
     if (contQuestion === 5) return <Redirect to="/Game/Feedback" />;
