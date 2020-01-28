ba1168e4dd88b892a19dfa71ce9efdefb27ed276
Henrique Eyer <henriqueeyer@outlook.com>
2020-01-27 21:28:51 -0300

    quase pronta

--- src/components/RankPlayer.jsx
+++ src/components/RankPlayer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './RankPlayer.css';
 
-const RankPlayer = ({ name, score, picture }) => (
+const RankPlayer = ({ index, name, score, picture }) => (
   <div className="RankPlayer">
-    <img src={picture} />
-    <p>
+    <img
+      src={picture}
+      data-testid={`profile-picture-${index}`}
+    />
+    <p data-testid={`${name}-${index}`}>
       {`${name} - ${score} Points`}
     </p>
   </div>
@@ -16,5 +20,6 @@ RankPlayer.propTypes = {
   name: PropTypes.string.isRequired,
   linkImage: PropTypes.string.isRequired,
   points: PropTypes.number.isRequired,
+  index: PropTypes.number.isRequired,
 };
 
--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -1,31 +1,24 @@
 import React from 'react';
 import RankPlayer from './RankPlayer';
 import { getRanking } from '../services/LocalStorage';
+import './Ranking.css';
 
-const compare = (a, b) => {
-  if (a.score > b.score) {
-    return 1;
-  }
-  if (a.score < b.score) {
-    return -1;
-  }
-  return 0;
-};
-
-const Ranking = () => {
-  return (
-    <div className="Ranking">
-      {getRanking()
-        .filter((player) => player.name)
-        .sort((a, b) => b.score - a.score)
-        .map(({
-          name,
-          score,
-          picture, }, index) => (
-            <RankPlayer key={index} name={name} score={score} picture={picture} />
-          ))}
-    </div>
-  )
-};
+const Ranking = () => (
+  <div className="Ranking">
+    <h1>Ranking</h1>
+    {getRanking()
+      .filter((player) => player.name)
+      .sort((a, b) => b.score - a.score)
+      .map(({ name, score, picture }, index) => (
+        <RankPlayer
+          index={index}
+          key={index}
+          name={name}
+          score={score}
+          picture={picture}
+        />
+      ))}
+  </div>
+);
 
 export default Ranking;
